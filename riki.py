2ac2c89f493a3a8d2b5e9e9df38eaa952332ce81
rishita217 <rishitasakharkar7@gmail.com>
2025-06-21 13:53:39 +0530

    initialize

--- riki.py
+++ riki.py
@@ -3,6 +3,10 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
+<<<<<<< HEAD
 # Example
+=======
+# Example usage
+>>>>>>> 8bd3bfa (Add riki.py)
 num = int(input("Enter a number: "))
 print(f"Factorial of {num} is {factorial(num)}")
